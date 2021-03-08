9affef5b2768127283e672d1aab9d85796eb7d8a
Alexandre Valente Rodrigues <alexandre.v.rodrigues@tecnico.ulisboa.pt>
2021-03-08 17:40:31 +0000

    Simplified anacron, parameterized script

--- remote-encrypted-backup.sh
+++ remote-encrypted-backup.sh
@@ -2,13 +2,15 @@
 #
 # This script was created to make Duplicity backups.
 # Full backups are done once every 3 months.
-# Then incremental backups are made on the other days.
+# Incremental backups are made every other day.
 
 # Check for the number of arguments
 if [ $# != 4 ]; then
 	echo "Usage: remote-backup <backup-code> <source> <target> <backup-frequency>"
 	exit 1
 fi
+
+LOG_DIR=/var/log/duplicity
 
 CODE=$1
 SOURCE=$2
@@ -17,22 +19,21 @@ FREQUENCY=$4
 
 # Flags to exclude some dotfiles
 if [ $CODE == "dotfiles" ]; then
-	FLAGS=" --exclude **/home/avalente/.cache/** \
-		--exclude **/home/avalente/.gem/** \
-		--exclude **/home/avalente/.local/share/lutris/** \
-		--exclude **/home/avalente/.local/lib/** \
-		--exclude **/home/avalente/.wine/** \
-		--exclude **/home/avalente/.local/share/libvirt/** \
-		--exclude **/home/avalente/.local/share/baloo/** \
-		--exclude **/home/avalente/.local/share/Steam/** \
-		--exclude **/home/avalente/.local/share/vocal/** \
-		--exclude **/home/avalente/.vagrant.d/** \
-		--include **/home/avalente/.** \
+	FLAGS=" --exclude **$HOME/.cache/** \
+		--exclude **$HOME/.gem/** \
+		--exclude **$HOME/.local/share/lutris/** \
+		--exclude **$HOME/.local/lib/** \
+		--exclude **$HOME/.wine/** \
+		--exclude **$HOME/.local/share/libvirt/** \
+		--exclude **$HOME/.local/share/baloo/** \
+		--exclude **$HOME/.local/share/Steam/** \
+		--exclude **$HOME/.local/share/vocal/** \
+		--exclude **$HOME/.vagrant.d/** \
+		--include **$HOME/.** \
 		--exclude **\*"
 elif [ $CODE == "documents" ]; then
-	FLAGS=" --exclude **/home/avalente/Documents/LEI** \
-		--exclude **/home/avalente/Documents/SSE-Drones**"
-
+	FLAGS=" --exclude **$HOME/Documents/LEI** \
+		--exclude **$HOME/Documents/SSE-Drones**"
 else
 	unset FLAGS
 fi
@@ -49,10 +50,10 @@ export MAILTO="avalente@protonmail.com"
 export MAIL_PASSPHRASE=`kwallet-query kdewallet -f Passwords -r mail-password`
 export MAIL_SENDER=`kwallet-query kdewallet -f Passwords -r mail-sender`
 
-# Backup files
+# --------- FOR DEBUGGING ------------
 # duplicity --dry-run --verbosity 8 --allow-source-mismatch --full-if-older-than $FREQUENCY $FLAGS $SOURCE $TARGET
-# duplicity full $FLAGS $SOURCE $TARGET
-duplicity --full-if-older-than $FREQUENCY $FLAGS $SOURCE $TARGET >> /var/log/duplicity/$CODE.log 2>&1
+# Backup files
+duplicity --full-if-older-than $FREQUENCY $FLAGS $SOURCE $TARGET >> $LOG_DIR/$CODE.log 2>&1
 
 if [ $? != 0 ]; then
 	MESSAGE="Error code: $?. $(date +'%d %b %Y %H:%M')-- Error performing the $CODE backup."
@@ -60,21 +61,21 @@ if [ $? != 0 ]; then
 	printf "Subject: Error performing the $CODE backup.\n\n" > /tmp/mail.dup
 	echo $MESSAGE >> /tmp/mail.dup
 	printf "\n\n------------------------------\n\n" >> /tmp/mail.dup
-	tail -n 100 /var/log/duplicity/$CODE.log >> /tmp/mail.dup
+	tail -n 100 $LOG_DIR/$CODE.log >> /tmp/mail.dup
 
 	ssmtp -au $MAIL_SENDER -ap $MAIL_PASSPHRASE $MAILTO < /tmp/mail.dup
 else
 	MESSAGE="$(date +'%d %b %Y %H:%M')-- Finished execution of remote $CODE backup."
 fi
 
-echo $MESSAGE >> /var/log/duplicity/$CODE.log 2>&1
+echo $MESSAGE >> $LOG_DIR/$CODE.log 2>&1
 
 notify-send -u normal "Remote $CODE Backup" $MESSAGE -t 15000
 
-# Deleting old backups - 6 months
+# Delete older backups then 3 months
 duplicity remove-older-than 3M \
 	--force \
-	$TARGET >> /var/log/duplicity/$CODE.log 2>&1
+	$TARGET >> $LOG_DIR/$CODE.log 2>&1
 
 # Unsetting the confidential variables so they are gone for sure.
 unset PASSPHRASE
